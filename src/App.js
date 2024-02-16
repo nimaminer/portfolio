f617918384f163a03e980d693b88a6cb160f3e63
Nima Sabzeh <sabzenima@gmail.com>
2024-02-16 20:39:26 +1100

    setup public domain route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
        <BrowserRouter>
       <Routes>
-          <Route index element={<HomePage/>} />
+          <Route path='/portfolio' element={<HomePage/>} />
           <Route path='/info' element={<InfoPage/>} />
 
       </Routes>
